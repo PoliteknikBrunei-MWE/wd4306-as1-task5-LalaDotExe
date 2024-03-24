df960d56287ab823119cffd51ee2031741d2c3cb
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-03-24 22:05:16 +0800

    Adding PlacesService

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Place } from './place.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlacesService {
+  private places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://www.thespruce.com/thmb/1',
+      250
+    ),
+    new Place(
+      'p2',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://www.thespruce.com/thmb/2',
+      189
+    ),
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://www.thespruce.com/thmb/3',
+      99
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 }
